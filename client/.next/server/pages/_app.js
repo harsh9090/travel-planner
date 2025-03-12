35ae06d276d7db72781fd808d934c9aba78606f3
Harsh <harsh.mithaiwala.hm@gmail.com>
2025-03-11 23:06:25 -0300

    updates

--- client/.next/server/pages/_app.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_app.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_4209fecb._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__bd1c6149._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/src/pages/_app.tsx [ssr] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/src/pages/_app.tsx [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- client/.next/server/pages/dashboard.js
+++
@@ -1,9 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/dashboard.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__f00d4c6b._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_next_ae3450b3._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_@mui_system_esm_96159101._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_@mui_material_47dbaf39._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_8d114cd7._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/dashboard.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/dashboard.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
